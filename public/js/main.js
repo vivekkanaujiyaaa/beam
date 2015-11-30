359794716ccd393ae46bf2e5fd70e407d5b99c67
Prashanth C A <prashanth.c.a@hp.com>
2015-11-30 09:27:37 +0530

    ASDAD

--- public/js/main.js
+++ public/js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
 		console.log(msg.message);
 	});
 	socket.on('linkuploadprogress', function(msg){
-		var fdetails = msg.message.split('#')[1].match(/(.*?) \% of (.*?)MB at (.*?) (\d+)s/),
+		var fdetails = msg.message.split('#')[1].match(/(.*?) \% of (.*?) at /),
 		mbc = $("#download-progress-label").text().match(/(.*?)MB\/(.*?)MB/)[2],
 		fcomp = Math.round(fdetails[1])*parseInt(mbc)/100;
 		$("#"+msg.hash+" #upload-progress-label").text(fcomp+" MB/"+fdetails[2]+" MB ("+Math.round(fdetails[1])+"%)");
